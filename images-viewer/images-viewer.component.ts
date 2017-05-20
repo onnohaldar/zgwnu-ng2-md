db001685ecf42ce116fb82f9be1867f572d87b4f
Onno Haldar <onno@zgw.nu>
2017-05-20 18:47:12 +0200

    refactor

---
+++ images-viewer/images-viewer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core'
+import { SafeResourceUrl } from '@angular/platform-browser'
+
+@Component({
+    selector: 'images-viewer',
+    templateUrl: 'images-viewer.component.html',
+    styleUrls: ['images-viewer.component.css']
+})
+export class ImagesViewerComponent implements OnInit {
+    
+    @Input() safeImageUrls: SafeResourceUrl[]
+
+    constructor() { }
+
+    ngOnInit() { 
+        console.log('ImagesViewerComponent.OnInit()')
+    }
+    
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export * from './datepicker/datepicker.module'
-
+export * from './images-viewer/images-viewer.component'
 export * from './toolbar/toolbar.component'
 export * from './toolbar/toolbar.service'
